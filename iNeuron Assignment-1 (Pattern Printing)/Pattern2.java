162261dc89e45338fbcc95384f39ee2b51501427
Attraya Das <attrayaghoshdas@gmail.com>
2023-01-10 22:34:49 +0530

    Add files via upload

---
+++ iNeuron Assignment-1 (Pattern Printing)/Pattern2.java
@@ -0,0 +1,24 @@
+/*
+ * Write a program to print:
+ *  1111
+ *  2222
+ *  3333
+ *  4444
+ */
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				{
+					System.out.print(i);
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
